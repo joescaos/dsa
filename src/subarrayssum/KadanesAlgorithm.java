a7d0f8286fc1f1432fc5d956cb476e1f96564249
joescaos <jxexcxo@gmail.com>
2023-05-02 21:27:42 -0500

    sum of subarrays kadanes algorithm

---
+++ src/subarrayssum/KadanesAlgorithm.java
@@ -0,0 +1,25 @@
+package subarrayssum;
+
+public class KadanesAlgorithm {
+
+    public static int kadanesSum(int[] arr) {
+        int currentSum = 0;
+        int largestSum = 0;
+        for (int j : arr) {
+            currentSum = currentSum + j;
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+            largestSum = Math.max(largestSum, currentSum);
+        }
+        return largestSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,10,11,12};
+        long startTime = System.nanoTime();
+        System.out.println(kadanesSum(arr));
+        long finalTime = System.nanoTime() - startTime;
+        System.out.println("Time of execution brute sum: " + finalTime);
+    }
+}
