0564960dc4d7d0715a99233dc6ccc81d55d5ac40
joescaos <jxexcxo@gmail.com>
2023-06-03 23:03:33 -0500

    recursive selection sort

--- src/selectionsort/SelectionSort.java
+++ src/selectionsort/SelectionSort.java
@@ -19,10 +19,28 @@ public class SelectionSort {
         }
     }
 
+    private static void swap(int[]arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void recursiveSelectionSort(int[] arr, int i, int n) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) min = j;
+        }
+        swap(arr, min, i);
+        if (i + 1 < n) recursiveSelectionSort(arr, i + 1, n);
+    }
+
     public static void main(String[] args) {
 
         int[] arr = new int[] {4, -2, 0, 5, 2, 7, 10};
-        selectionSort(arr);
+        //selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        recursiveSelectionSort(arr, 0, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }
